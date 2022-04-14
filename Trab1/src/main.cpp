a8c482caff368c84d4832cb519d073ed161e2d39
samue1v <samue1v@github.com>
2022-04-14 19:04:58 -0300

    connected components of a graph - still hava to test

--- Trab1/src/main.cpp
+++ Trab1/src/main.cpp
@@ -5,30 +5,31 @@
 #include "../headers/Graph.h"
 void parseInput(Graph*);
 void BFS(Graph*, Vertex*,int);
-void showResult(std::vector<Vertex*>*);
+void showResult(Graph*);
 int main(int argc, char const *argv[])
 {
     Graph *g = new Graph;
     parseInput(g);
     int color = 0;
     for(int i = 0;i<g->vecVertexSize();i++){
-        
         Vertex *v = g->getVertex(i);
         if(v->color == 0){    
             BFS(g,v,++color);
         }
     }
+    showResult(g);
+    /*
     for(int i=0;i<g->vecEdgeSize();i++){
         std::cout 
         << "label:" <<g->getEdge(i).edge.first->label << " color:" << g->getEdge(i).edge.first->color << " " 
         << "label:" <<g->getEdge(i).edge.second->label << " color:"<<g->getEdge(i).edge.second->color<<"\n"    ;
-    }
+    }*/
+    
 }
 
 void parseInput(Graph* g){
     char in[100];
     int n1,v1,v2;
-    g->addVertex(0);
     std::fgets(in,sizeof(in),stdin);//dl
     std::fgets(in,sizeof(in),stdin);//edgelist1
     std::fgets(in,sizeof(in),stdin);//n=10\n
@@ -39,19 +40,19 @@ void parseInput(Graph* g){
     std::fgets(in,sizeof(in),stdin);//data:
     while(std::fgets(in,sizeof(in),stdin)!=NULL){
         if(std::sscanf(in,"%d %d\n",&v1,&v2)!=2){
-            std::cout <<"gay\n";
             break;
         }
-        g->addEdge(g->getVertex(v1),g->getVertex(v2));
+        g->addEdge(g->getVertex(v1-1),g->getVertex(v2-1));
 
         
     }
-    //std::cout <<"gay\n";
-        for(int i=0;i<g->vecEdgeSize();i++){
+    /*
+    for(int i=0;i<g->vecEdgeSize();i++){
         std::cout 
         << g->getEdge(i).edge.first->label << " " << g->getEdge(i).edge.first->color << " " 
         << g->getEdge(i).edge.second->label << " "<< g->getEdge(i).edge.second->color<<"\n"    ;
     }
+    */
 }
 
 void BFS(Graph* g,Vertex* v,int color){
@@ -60,7 +61,6 @@ void BFS(Graph* g,Vertex* v,int color){
     q.push(v);
     while(q.size()>0){
         v1 = q.front();
-        std::cout << v1->label <<"\n";
         v1->color=color;
         q.pop();
         for(int i=0;i<v1->neighbours.size();i++){
@@ -71,7 +71,18 @@ void BFS(Graph* g,Vertex* v,int color){
     }
 }
 
-void showResult(std::vector<Vertex*>* vecVertices){
-    
+void showResult(Graph *g){
+    for(int i = 0;i<g->vecVertexSize();i++){
+        if(g->getVertex(i)->done == false){
+            int color = g->getVertex(i)->color;
+            for(int j = 0;j<g->vecVertexSize();j++){
+                if(g->getVertex(j)->done == false && g->getVertex(j)->color == color){
+                    std::cout << g->getVertex(j)->label << " ";  
+                    g->getVertex(j)->done = true;
+                }
+            }
+            std::cout << "\n"; 
+        }
+    }
 }
 
